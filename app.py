24d95dcba082d9664dd05d908b67a561c4919ff5
Fabio Murillo <fabiomurillo2506@gmail.com>
2025-04-18 13:08:12 -0400

    added navigation features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db, Recipes, Users
 
 app = Flask(__name__)
@@ -30,6 +30,16 @@ def get_users():
             f"ID: {user.userid} | Username: {user.username} | Email: {user.email} | Name: {user.firstname} {user.lastname} | Creation Date: {user.created_at}"
         )
     return "<br>".join(output)
+
+@app.route("/search")
+def search():
+    query = request.args.get("query")
+
+    if query.isdigit():
+        results = Recipes.query.filter_by(recipeid=int(query)).all()
+    else:
+        results = Recipes.query.filter(Recipes.name.ilike(f"%{query}%")).all()
+    return render_template("search_results.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
